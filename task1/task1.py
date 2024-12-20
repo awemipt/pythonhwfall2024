4fcf959d1cf59748167d17100ea26677f08e76ea
awe4fork <zadiran.nv@phystech.edu>
2024-12-20 22:54:34 +0300

    task1done, requrenment upd

--- task1/task1.py
+++ task1/task1.py
@@ -4,8 +4,10 @@ from sklearn import datasets
 from typing import Tuple
 import numpy as np 
 
+from matplotlib import pyplot as plt
+np.random.seed(10)
 def task1_1(df: DataFrame) -> DataFrame:
-    shuffled_df = df.sample(frac=1) 
+    shuffled_df = df.sample(frac=1,random_state=1)
     return shuffled_df
 
 
@@ -30,16 +32,48 @@ def task1_4_5(X_test: DataFrame, X_train: DataFrame,) -> np.array:
     distances = np.sqrt(np.sum(squared_diff, axis=2))
     return distances
 
-def task1_6_7_8(distances: np.array, y_train: Series,y_test: Series, n=5) -> np.array:
+def task1_6_7_8(distances: np.array, y_train: Series,X_test: Series, n=5) -> Series:
     res = {}
-    for test_sample, y_test_index in zip(distances, y_test.index):
-        indices = np.argpartition(test_sample, n)[:n]
-        sorted_indices = indices[np.argsort(test_sample[indices])]
-        res.update({y_test_index: y_train.iloc[sorted_indices].mode().values[0]})
-    return Series(res)
+    for test_sample, x_test_index in zip(distances, X_test.index):
+        indices = np.argsort(test_sample)
+        res.update({x_test_index: y_train.iloc[indices[:n]].mode().values[0]})
         
+
+    return Series(res)
+def task1_9(y_train:Series):
+    return y_train.copy()
+
 def task1_10_11(y_pred: Series, y_test:Series):
-    pass
+    score = 0
+    for pred, test in zip(y_pred.values, y_test.values):
+        if pred==test:
+            score += 1
+    return score / len(y_pred)
+def task1_12(df):
+
+    train, test = task1_2(df, part=.2)
+    X_train, y_train, X_test, y_test = task1_3(train, test)
+    distances = task1_4_5(X_test, X_train)
+    predicted = task1_6_7_8(distances, y_train, X_test)
+    score = task1_10_11(predicted, y_test)
+    return score
+class KNN:
+    def __init__(self, neighbours:int=4):
+        self.neighbours = neighbours
+    
+    def fit(self, X_train: DataFrame, y_train: DataFrame):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def predict(self, X_test):
+        distances = task1_4_5(X_test, self.X_train)
+        res = task1_6_7_8(distances, self.y_train, X_test, n=self.neighbours)
+        self.res = res
+        return res
+    
+    def score(self, y_test):
+        return task1_10_11(self.res, y_test)
+    
 
 def main():
     iris = datasets.load_iris()
@@ -54,12 +88,33 @@ def main():
     tasks_res.append(tast1_3res)
 
 
-    X_train, y_train, X_test, y_test = task1_3(train, test)
-    print(y_test)
+    X_train, y_train, X_test, y_test = tast1_3res
+
     task1_4_5res = task1_4_5(X_test, X_train)
     tasks_res.append(task1_4_5)
 
-    task1_6_7_8_res = task1_6_7_8(task1_4_5res, y_train, y_test)
+    task1_6_7_8res = task1_6_7_8(task1_4_5res, y_train, X_test)
+    tasks_res.append(task1_6_7_8res)
+    task1_10_11res = task1_10_11(task1_6_7_8res, y_test)
+    print(f"aqquracy 80% train { task1_10_11res}")
+    tasks_res.append(task1_10_11res)
+
+    task1_12res = task1_12(task1_1res)
+
+    print(f"aqquracy 20% train { task1_10_11res}")
+    tasks_res.append(task1_12res)
+    
+    task1_19res = []
+    for n in range(1, 10):
+        knn = KNN(neighbours=n)
+        knn.fit(X_train, y_train)
+        knn.predict(X_test)
+        task1_19res.append((n, knn.score(y_test)))
+    x, y = zip(*task1_19res)
+    print(x)
+    print(y)
+    plt.plot(x, y)
+    plt.savefig("1.jpg")
     
 if __name__ == "__main__":
     main()
